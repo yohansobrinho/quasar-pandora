997ac3bd8f9a31ef985ebc43e29a6daba1506254
Yohan Sobrinho <yohan.sobrinho@w3as.com.br>
2019-06-28 15:25:03 -0300

    Altualização de pattern e jpgs

--- src/pages/Aframe.vue
+++ src/pages/Aframe.vue
@@ -5,6 +5,14 @@
          <a-plane src="statics/caixa/1.jpeg" scale="6.3 6.3 6.3" rotation="-90 90 0" position="0 0 -0.8" transparent="true">
           </a-plane>
       </a-marker>
+      <a-marker id='frac3' type='pattern' url='statics/marker/2.patt' vidhandler>
+         <a-plane src="statics/caixa/2.jpg" scale="6.3 6.3 6.3" rotation="-90 90 0" position="0 0 -0.8" transparent="true">
+          </a-plane>
+      </a-marker>
+      <a-marker id='frac3' type='pattern' url='statics/marker/3.patt' vidhandler>
+         <a-plane src="statics/caixa/3.jpg" scale="6.3 6.3 6.3" rotation="-90 90 0" position="0 0 -0.8" transparent="true">
+          </a-plane>
+      </a-marker>
         <a-entity camera></a-entity>
     </a-scene>
 
